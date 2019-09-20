48970e2f337b3b1217a01975329644d88915f5e3
Administrator <787209168@qq.com>
2019-09-20 20:52:15 +0800

    Initial commit

---
+++ web-shopcar/src/main/java/com/vip/webshopcar/WebShopcarApplication.java
@@ -0,0 +1,13 @@
+package com.vip.webshopcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopcarApplication.class, args);
+    }
+
+}
